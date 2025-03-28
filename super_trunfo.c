859e8360370815ac91e6d100f65e61637c76ea24
vitorsansoni2 <vitorsansonii@gmail.com>
2025-03-28 21:50:57 +0000

    desafio super trunfo carta um

---
+++ super_trunfo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+// Criação carta 1
+
+char codigo[20] = "A01";
+char nome_da_cidade[20] = "Taubate";
+int população = 321299;
+float área = 625.004;
+float PIB = 50563.09;
+int pontoturistico = 6;
+
+// Inicializando variaveis
+
+printf ("Codigo do Estado é: \n");
+printf ("Nome da Cidade é: \n");
+printf ("A População é: \n");
+printf ("A Área é: \n");
+printf ("O PIB é:  \n");
+printf ("Numero de Pontos Turisticos é: \n");
+
+// Lendo os dados da carta 1
+
+
+return 0;
+
+}
