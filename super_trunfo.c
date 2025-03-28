60460f28a9db5b1e43e32f9f6b6d0f80e8176a6b
vitorsansoni2 <vitorsansonii@gmail.com>
2025-03-28 22:18:04 +0000

    tentando rodar o codigo

--- super_trunfo.c
+++ super_trunfo.c
@@ -3,11 +3,12 @@
 int main() {
 
 // Criação carta 1
+// Definindo variaveis para as cartas um e dois
 
 char codigo[20] = "A01";
 char nome_da_cidade[20] = "Taubate";
-int população = 321299;
-float área = 625.004;
+int populacao = 321.299;
+float Area = 625.004;
 float PIB = 50563.09;
 int pontoturistico = 6;
 
@@ -15,12 +16,21 @@ int pontoturistico = 6;
 
 printf ("Codigo do Estado é: \n");
 printf ("Nome da Cidade é: \n");
-printf ("A População é: \n");
-printf ("A Área é: \n");
+printf ("A Populacao é: \n");
+printf ("A Area é: \n");
 printf ("O PIB é:  \n");
 printf ("Numero de Pontos Turisticos é: \n");
 
-// Lendo os dados da carta 1
+// Exibindo os valores
+
+scanf ("%s" , codigo);
+scanf ("%s" , nome_da_cidade);
+scanf ("%i" , populacao);
+scanf ("%f" , Area);
+scanf ("%f" , PIB);
+scanf ("%i" , pontoturistico);
+
+// leitura dos valores carta Um
 
 
 return 0;
